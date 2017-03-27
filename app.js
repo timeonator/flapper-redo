b9125b6c5c2c2dfed4aab06ebdc63398ace24a47
Robert Gifford <timeonator@gmail.com>
2017-03-26 20:53:23 -0700

    able to read and write posts

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -9,6 +13,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
