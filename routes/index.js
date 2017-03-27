7fa6e876b80150dd0102c70d596655b42fe47ba9
Robert Gifford <timeonator@gmail.com>
2017-03-27 09:34:30 -0700

    dded upvotes to posts

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,12 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
 module.exports = router;
