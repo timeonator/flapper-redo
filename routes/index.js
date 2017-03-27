f7abb004fdc443176b34054d365698debeb296ab
Robert Gifford <timeonator@gmail.com>
2017-03-27 08:48:12 -0700

    dded preloading for posts

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,29 @@
+var express = require('express');
+var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
-var express = require('express');
-var router = express.Router();
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
 
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
 
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
